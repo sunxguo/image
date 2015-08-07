0c67ad2aa5cd4392f04bfbbde6c0f2e48adf1668
MonkeyKing <MonkeyKing1024@gmail.com>
2015-08-07 14:47:05 +0800

    update

--- upload.php
+++ upload.php
@@ -1,19 +1,23 @@
 <?php
 //	error_reporting(E_ERROR | E_WARNING);
  	//原文件名
-	$file_name = $_FILES["file"]['name'];
+	echo $file_name = $_FILES["image"]['name'].'<br>';
 	//服务器上临时文件名
-	$tmp_name = $_FILES["file"]['tmp_name'];
+	echo $tmp_name = $_FILES["image"]['tmp_name'].'<br>';
 	//文件大小
-	$file_size = $_FILES["file"]['size'];
-	move_uploaded_file($_FILES["file"]["tmp_name"],$_FILES["file"]["name"]);
-	echo $_FILES["file"]["name"];
-/*
-	$im = new Imagick($_FILES["file"]["name"]);
-	$im->setImageIndex(0);
-	$im->setIteratorIndex(0);
-	$im->stripImage(); //去除图片信息
-	$im->setImageCompressionQuality(80); //图片质量
-	$im->writeImage('upload.jpg');
-	echo "<img src='upload.jpg'/>";*/
+	echo $file_size = $_FILES["image"]['size'].'<br>';
+	if (file_exists($file_name)){
+		echo  $file_name. "已经存在，请更换文件名. ";
+    }
+    else{
+		move_uploaded_file($tmp_name,$file_name);
+		$im = new Imagick($file_name);
+		$im->setImageIndex(0);
+		$im->setIteratorIndex(0);
+		$im->stripImage(); //去除图片信息
+		$im->setImageCompressionQuality(80); //图片质量
+		$im->writeImage('upload.jpg');
+		echo "<img src='upload.jpg'/>";
+    }
+	
 ?>
